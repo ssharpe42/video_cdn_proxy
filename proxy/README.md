0fec7259cd62c44a609eca8f42a5e4dbf17bdef5
ssharpe42 <ssharpe42y@gmail.com>
2019-11-04 15:19:52 -0500

    client read first

--- proxy/README.md
+++ proxy/README.md
@@ -1,4 +1,41 @@
+Notes:
+
+Currently browser needs to be refreshed 3 times to start streaming. It doesn't
+seem to request after receiving objects. 
+
+
 Files:
 
-- socket_utils.py - helper functions
-- proxy - implements and runs proxy server
+socket_utils.py
+---------------
+
+receive - Receive messages from connections
+
+socket_bind_listen -  bind to an address and port, then listen
+
+socket_bind_connect -  bind to a fake address and port and connect to server
+
+
+vid_utils.py
+-------------
+
+bits - Calculate bits in message
+
+calc_tp - Calculate throughput
+
+ewma - Calculate exponential moving average
+
+is_vid_request - Check if a video fragment name is inside string
+
+get_chunkname - Get the name of the video chunk
+
+get_req_bitrate - Get the requested bit rate
+
+parse_f4m - Parse the f4m file for available bitrates
+
+modify_uri_bitrate - Modify the requested bitrate
+    
+
+proxy
+------
+Proxy - proxy class that implements a proxy server
--- proxy/vid_utils.py
+++ proxy/vid_utils.py
@@ -2,38 +2,64 @@ import re
 
 
 def bits(s):
+    """Calculate bits in message"""
     return len(s) * 8.0
 
 
 def calc_tp(ts, tf, n_bits):
+    """
+    Calculate throughput
+    :param ts: start time
+    :param tf: end time
+    :param n_bits: bits transferred
+    :return: kb/s
+    """
     return n_bits / 1000.0 / (tf - ts)
 
 
 def ewma(current, new, alpha):
+    """
+    Calculate exponential moving average
+    :param current: current estimate
+    :param new: new sample
+    :param alpha: alpha
+    :return: new estimate
+    """
 
     return alpha * new + current * (1 - alpha)
 
 
 def is_vid_request(string):
+    """Check if a video fragment name is inside string"""
     return bool(re.search('\/vod\/[0-9]+Seg[0-9]+-Frag[0-9]+', string))
 
 
 def get_chunkname(request):
+    """Get the name of the video chunk"""
     return re.search(r'\/vod\/[0-9]+Seg[0-9]+-Frag[0-9]+', request).group()
 
 
 def get_req_bitrate(request):
+    """Get the requested bit rate"""
     return re.search('(?<=\/vod\/)[0-9]+', request).group()
 
 
 def parse_f4m(data):
-    # media = re.search('(?<=<media)((.|\n|\r)*)>', data)
+    """Parse the f4m file for available bitrates"""
     bitrates = re.findall('(?<=bitrate=\")[0-9]+(?=\")', data)
 
     return [int(b) for b in bitrates]
 
 
 def modify_uri_bitrate(request, available_bitrates, tp_estimate):
+
+    """
+    Modify the requested bitrate
+    :param request: original client request
+    :param available_bitrates: available bitrates
+    :param tp_estimate: current throughput estimate
+    :return: new request
+    """
 
     allowed_bitrates = [b for b in
                        available_bitrates if b <= tp_estimate / 1.5]
